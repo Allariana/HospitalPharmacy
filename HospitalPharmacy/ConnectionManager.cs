5c1f695e4e37b1f1c90094c20e32df835162c3fd
Allariana <kinga.sochacka08@gmail.com>
2019-11-08 15:10:32 +0100

    Create connection to the database

---
+++ HospitalPharmacy/ConnectionManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalPharmacy
+{
+    class ConnectionManager
+    {
+        public SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=Pharmacy;Integrated Security=True");
+
+        public bool verifyIfUserIsCorrect(String username, String password)
+        {
+            connection.Open();
+            String command = "select * from Users where Username = '"
+                + username + "' AND Password = '" + password + "';";
+            SqlDataAdapter adapter = new SqlDataAdapter(command, connection);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            bool result = false;
+            foreach (DataRow r in dt.Rows)
+            {
+                result = true;
+            }
+            connection.Close();
+            return result;
+        }
+    }
+}
